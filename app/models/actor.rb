bd873c661801d52ed7c34c9fc75af3a8e443fc1f
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-29 05:01:37 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,8 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    "#{self.name}"
+    characters.each do |character|
+      "#{character.name} #{character.show.name}"
+    end 
   end
 end
